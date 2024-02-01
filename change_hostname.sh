d0c0b5c34c162fc4ed60f0daadae7ab79c556612
htazq <61930738+htazq@users.noreply.github.com>
2024-02-01 20:17:05 +0800

    Update change_hostname.sh

--- change_hostname.sh
+++ change_hostname.sh
@@ -19,14 +19,17 @@ memory_size=$(lsmem | awk '/^Total online memory:/ {print $4}')
 # 获取硬盘大小
 disk_size=$(lsblk | awk '/disk/ {print $4}')
 
-# 获取公共 IPV4 地址
+# 获取公共 IP 地址
 public_ip=$(curl -sS4 ip.sb)
 
 # 判断所处地区
 region=$(curl -s "https://ipinfo.io/${public_ip}/json" | jq -r .region)
 
+# 将IP中的点替换为横杠，生成有效的主机名
+ip_for_hostname=$(echo "${public_ip}" | tr '.' '-')
+
 # 组合新的主机名
-new_hostname="${cpu_cores}-${memory_size}-${disk_size}-${region}-${public_ip}"
+new_hostname="${cpu_cores}-${memory_size}-${disk_size}-${region}-${ip_for_hostname}"
 
 # 更改主机名
 echo "Setting new hostname: ${new_hostname}"

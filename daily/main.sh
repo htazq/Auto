b490a6c95a37295b319a4cb1932875f8847c7d2e
htazq <61930738+htazq@users.noreply.github.com>
2024-04-20 21:58:27 +0800

    Update main.sh

--- daily/main.sh
+++ daily/main.sh
@@ -10,10 +10,4 @@ touch "$LOG_FILE"    # 创建日志文件
 
 echo "每日巡检开始！"
 
-#全自动解决vi方向键乱跳
-bash <(wget -qO- -o- https://raw.githubusercontent.com/htazq/Auto/main/Auto-Vi.sh)
-
-#全自动修改主机名称为：cpu核心数-内存容量-硬盘容量-服务器地区-服务器IP
-bash <(wget -qO- -o- https://raw.githubusercontent.com/htazq/Auto/main/change_hostname.sh)
-
 echo "每日巡检结束！" >> "$LOG_FILE"

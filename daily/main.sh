21dcafc46e184484e230466cf5cbc950d3cb52f1
htazq <61930738+htazq@users.noreply.github.com>
2024-04-20 22:06:03 +0800

    Update main.sh

--- daily/main.sh
+++ daily/main.sh
@@ -7,7 +7,10 @@ LOG_FILE="$LOG_DIR/$(date +'%Y-%m-%d_%H-%M-%S').log"
 mkdir -p "$LOG_DIR"  # 创建日志目录
 touch "$LOG_FILE"    # 创建日志文件
 # 输出日志信息到日志文件
+exec &>> "$LOG_FILE"
 
 echo "每日巡检开始！"
 
-echo "每日巡检结束！" >> "$LOG_FILE"
+#待补充
+
+echo "每日巡检结束！"
